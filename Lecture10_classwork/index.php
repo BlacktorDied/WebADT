82c404f75531b05ad7c3b860d1e228eb1e4fe2bc
BlacktorDied <Alex.dozorin@gmail.com>
2024-11-19 13:24:02 +0200

    Added Lacture10 classwork file:

---
+++ Lecture10_classwork/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP date</title>
+</head>
+<body>
+    <div>
+        <h1>PHP date</h1>
+        <footer>
+            <p>
+                &copy; 
+                <?php
+                    echo date("Y-m-d H:i:s");
+                ?>
+            </p>
+        </footer>
+    </div>
+</body>
+</html>
